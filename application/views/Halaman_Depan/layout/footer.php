8f3c7f8cfe9c095f7569c89e56f0f50d2e3137db
AbdanGit <muhammadabdan918@gmail.com>
2023-06-24 23:02:25 +0800

    footer di perbaiki

--- application/views/Halaman_Depan/layout/footer.php
+++ application/views/Halaman_Depan/layout/footer.php
@@ -1,59 +1,64 @@
-    <!--============================= FOOTER =============================-->
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="foot-logo">
-                        <a href="<?php echo site_url(); ?>">
-                            <img src="<?php echo base_url() . 'theme/images/logo-hannas.png' ?>" class="img-fluid" alt="footer_logo">
-                        </a>
-                        <p><?php echo date('Y'); ?> © copyright by <a href="#" target="_blank">Nadifa Nabila & Abdan</a>. <br>All rights reserved.</p>
-                    </div>
+<!--============================= FOOTER =============================-->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="foot-logo">
+                    <a href="<?php echo site_url(); ?>">
+                        <img src="<?php echo base_url() . 'theme/images/logo-hannas.png' ?>" class="img-fluid"
+                            alt="footer_logo">
+                    </a>
+                    <p>
+                        <?php echo date('Y'); ?> © copyright by <a href="#" target="_blank">Nadifa Nabila & Abdan</a>.
+                        <br>All rights reserved.
+                    </p>
                 </div>
-                <div class="col-md-6">
-                    <div class="sitemap">
-                        <h3>Menu Utama</h3>
-                        <ul>
-                            <li><a href="<?php echo site_url(); ?>">Home</a></li>
-                            <li><a href="<?php echo site_url('tentang'); ?>">Tentang Kami</a></li>
-                            <li><a href="<?php echo site_url('produk'); ?>">Produk </a></li>\
-                        </ul>
-                    </div>
+            </div>
+            <div class="col-md-3">
+                <div class="sitemap">
+                    <h3>Menu Utama</h3>
+                    <ul>
+                        <li><a href="<?php echo site_url(); ?>">Home</a></li>
+                        <li><a href="<?php echo site_url('tentang'); ?>">Tentang Kami</a></li>
+                        <li><a href="<?php echo site_url('produk'); ?>">Produk </a></li>\
+                    </ul>
                 </div>
-                <div class="col-md-3">
-                    <div class="address">
-                        <h3>Hubungi Kami</h3>
-                        <p><span>Alamat: </span> Banjarmasin Kota, Jl. Lkr. Dalam Selatan Ruko Abu Coklat No.84, RT.27/RW.02</p>
-                        <p>Email : hannasbjm@gmail.com
-                            <br> Phone : +62 813-5119-2539
-                        </p>
-                        <ul class="footer-social-icons">
-                            <li><a href="#"><i class="fa fa-instagram fa-fb" aria-hidden="true"></i></a></li>
-                            <li><a href="#"><i class="fa fa-linkedin fa-in" aria-hidden="true"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter fa-tw" aria-hidden="true"></i></a></li>
-                        </ul>
-                    </div>
+            </div>
+            <div class="col-md-6">
+                <div class="address">
+                    <h3>Hubungi Kami</h3>
+                    <p><span>Alamat: </span> Banjarmasin Kota, Jl. Lkr. Dalam Selatan Ruko Abu Coklat No.84, RT.27/RW.02
+                    </p>
+                    <p>Email : hannasbjm@gmail.com
+                        <br> Phone : +62 813-5119-2539
+                    </p>
+                    <ul class="footer-social-icons">
+                        <li><a href="#"><i class="fa fa-instagram fa-fb" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-linkedin fa-in" aria-hidden="true"></i></a></li>
+                        <li><a href="#"><i class="fa fa-twitter fa-tw" aria-hidden="true"></i></a></li>
+                    </ul>
                 </div>
             </div>
         </div>
-    </footer>
-    <!--//END FOOTER -->
-    <!-- jQuery, Bootstrap JS. -->
+    </div>
+</footer>
+<!--//END FOOTER -->
+<!-- jQuery, Bootstrap JS. -->
 
-    <script src="<?php echo base_url() . 'theme/js/jquery.min.js' ?>"></script>
-    <script src="<?php echo base_url() . 'theme/js/tether.min.js' ?>"></script>
-    <script src="<?php echo base_url() . 'theme/js/bootstrap.min.js' ?>"></script>
-    <!-- Plugins -->
-    <script src="<?php echo base_url() . 'theme/js/slick.min.js' ?>"></script>
-    <script src="<?php echo base_url() . 'theme/js/waypoints.min.js' ?>"></script>
-    <script src="<?php echo base_url() . 'theme/js/counterup.min.js' ?>"></script>
-    <script src="<?php echo base_url() . 'theme/js/owl.carousel.min.js' ?>"></script>
-    <script src="<?php echo base_url() . 'theme/js/validate.js' ?>"></script>
-    <script src="<?php echo base_url() . 'theme/js/tweetie.min.js' ?>"></script>
-    <!-- Subscribe -->
-    <script src="<?php echo base_url() . 'theme/js/subscribe.js' ?>"></script>
-    <!-- Script JS -->
-    <script src="<?php echo base_url() . 'theme/js/script.js' ?>"></script>
-    </body>
+<script src="<?php echo base_url() . 'theme/js/jquery.min.js' ?>"></script>
+<script src="<?php echo base_url() . 'theme/js/tether.min.js' ?>"></script>
+<script src="<?php echo base_url() . 'theme/js/bootstrap.min.js' ?>"></script>
+<!-- Plugins -->
+<script src="<?php echo base_url() . 'theme/js/slick.min.js' ?>"></script>
+<script src="<?php echo base_url() . 'theme/js/waypoints.min.js' ?>"></script>
+<script src="<?php echo base_url() . 'theme/js/counterup.min.js' ?>"></script>
+<script src="<?php echo base_url() . 'theme/js/owl.carousel.min.js' ?>"></script>
+<script src="<?php echo base_url() . 'theme/js/validate.js' ?>"></script>
+<script src="<?php echo base_url() . 'theme/js/tweetie.min.js' ?>"></script>
+<!-- Subscribe -->
+<script src="<?php echo base_url() . 'theme/js/subscribe.js' ?>"></script>
+<!-- Script JS -->
+<script src="<?php echo base_url() . 'theme/js/script.js' ?>"></script>
+</body>
 
-    </html>
+</html>
